f7a44ceed935ce075e3ef19d4c4c4b3fc9f93d6d
Clare Jackson <clare-jackson@lambdastudents.com>
2021-03-21 17:58:29 -0400

    two tests done

--- api/server.test.js
+++ api/server.test.js
@@ -7,18 +7,30 @@ test('sanity', () => {
 })
 
 describe("users integration tests", () => {
-  it("creates a new user", async () => {
-    const res = await supertest(server)
-    .post("/api/auth/register")
-    .send({
-      username: "clare",
-      password: "jackson"
-    })
-    expect(res.statusCode).toBe(201)
-    expect(res.type).toBe("application/json")
-    expect(res.body.id).toBeDefined()
-    expect(res.body.username).toBe("clare")
+  describe("/api/auth/register", () => {
+    it("creates a new user", async () => {
+      const res = await supertest(server)
+      .post("/api/auth/register")
+      .send({
+        username: "clare",
+        password: "jackson"
+      })
+      expect(res.statusCode).toBe(201)
+      expect(res.type).toBe("application/json")
+      expect(res.body.id).toBeDefined()
+      expect(res.body.username).toBe("clare")
+   })
+   it("returns correct json error message", async () => {
+     const res = await supertest(server)
+     .post("/api/auth/register")
+     .send({
+       username: "",
+       password: "jackson"
+     })
+     expect(res.statusCode).toBe(409)
+     expect(res.body.message).toBe("username and password required")
+     
+   })
   })
-  
 })
 
