e7910478346a3d3b3cd293d8002075eea4d72d8f
Clare Jackson <clare-jackson@lambdastudents.com>
2021-03-21 17:45:46 -0400

    one test

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,24 @@
 // Write your tests here
+const supertest = require('supertest');
+const server = require('./server');
+
 test('sanity', () => {
   expect(true).toBe(false)
 })
+
+describe("users integration tests", () => {
+  it("creates a new user", async () => {
+    const res = await supertest(server)
+    .post("/api/auth/register")
+    .send({
+      username: "clare",
+      password: "jackson"
+    })
+    expect(res.statusCode).toBe(201)
+    expect(res.type).toBe("application/json")
+    expect(res.body.id).toBeDefined()
+    expect(res.body.username).toBe("clare")
+  })
+  
+})
+
